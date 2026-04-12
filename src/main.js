84ba7fed7d608aa5d74f3b751edfdc7a9a64f8b1
Uwe Kamper <me@uwekamper.de>
2026-04-12 23:25:07 +0200

    redisgn the form

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import './style.css'
-import * as FilePond from 'filepond';
+import {create, registerPlugin} from 'filepond';
+// Import the Image Preview plugin
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 
 // Get a reference to the file input element
 const inputElement = document.querySelector('input[type="file"]');
 
 // Create a FilePond instance
-const pond = FilePond.create(
+registerPlugin(FilePondPluginImagePreview);
+
+const pond = create(
   inputElement,
   {
     credits: [],
+    allowMultiple: true,
+    // disabled: true,
+    imagePreviewMinHeight: 64,
+    imagePreviewMaxHeight: 128,
   }
 );
